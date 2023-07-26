a96d8c602bf4580e07e172aa136820c08f9f1a21
Arezki-Nadji <arezki.m.nadji96@gmail.com>
2023-07-26 23:10:47 +0200

    added Delete edit and get for drink detail

--- drinks/urls.py
+++ drinks/urls.py
@@ -4,5 +4,6 @@ from drinks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('drinks/', views.drink_list, name="drink_list")
+    path('drinks/', views.drink_list, name="drink_list"),
+    path('drinks/<int:id>', views.drink_detail, name="drink_detail")
 ]
--- drinks/views.py
+++ drinks/views.py
@@ -7,17 +7,41 @@ from rest_framework import status
 
 @api_view(['GET', 'POST'])
 def drink_list(request):
+
     if request.method == 'GET':
-        #get all the drinks
+        # get all the drinks
         drinks = Drinks.objects.all()
-
-        #serialize them
+        # serialize them
         serializer = DrinkSerializer(drinks, many = True)
-
-        #return json
-        return JsonResponse({ "drinks ": serializer.data }, safe = False)
+        # return json
+        return Response(serializer.data)
+    
     if request.method == 'POST':
         serializer = DrinkSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE']) 
+def drink_detail(request, id):
+
+    try:
+        drink = Drinks.objects.get(pk=id)
+    except Drinks.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serialiazer = DrinkSerializer(drink)
+        return Response(serialiazer.data)
+    
+    elif request.method == 'PUT':
+        serialiazer = DrinkSerializer(drink, data=request.data)
+        if serialiazer.is_valid():
+            serialiazer.save()
+            return Response(serialiazer.data)
+        return Response(serialiazer.errors, status=status.HTTP_404_NOT_FOUND)
+    
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
